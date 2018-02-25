894cd908e0e590520db2601015ac59ff6ef7380b
kaminska4nna <kaminska4nna@users.noreply.github.com>
2018-02-25 12:32:41 +0100

    25.02.18.

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,15 +13,21 @@ import java.util.Hashtable;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+
 
     public String buildTrelloCardEmail(String message){
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("task_url", "https://morning-springs-79162.herokuapp.com/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye","Best regards!" );
+        context.setVariable("company_name", adminConfig.getCompanyName());
         return templateEngine.process("mail/created-trello-card-mail", context);
-
-
-
     }
 }
