84b42c3b6484dec70a0b0fc52ba083e16d49f62a
kaminska4nna <kaminska4nna@users.noreply.github.com>
2018-02-26 16:23:27 +0100

    26.02.18.

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+    @CrossOrigin("*")
     @RestController
     @RequestMapping("/v1/trello")
-    @CrossOrigin("*")
     public class TrelloController {
 
         @Autowired
